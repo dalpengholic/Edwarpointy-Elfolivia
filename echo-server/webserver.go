b3b1843b60b58ad644bec27e9a510d825f60b2e0
dalpengholic <dalpengholic@gmail.com>
2023-03-16 16:48:50 +0200

    Refactoring1

--- echo-server/webserver.go
+++ echo-server/webserver.go
@@ -4,18 +4,21 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+        "os"
 )
 
 // Define struct to hold data of `flag`
 type EchoConfig struct {
 	port_number string
 	wel_message string
+	hostname string
 }
 
 // Initialization of variables
 func InitConfig(param EchoConfig) EchoConfig {
 	flag.StringVar(&param.port_number, "p", "8000", "default port number")
 	flag.StringVar(&param.wel_message, "c", "hello", "default message")
+	param.hostname, _ = os.Hostname()
 	flag.Parse()
 
 	return param
@@ -27,7 +30,7 @@ func main() {
 	holder.port_number = ":" + holder.port_number
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, holder.wel_message)
+		fmt.Fprintln(w, holder.wel_message, "from", holder.hostname)
 	})
 
 	http.ListenAndServe(holder.port_number, nil)
